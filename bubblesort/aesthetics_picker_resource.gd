34e3c043cdfa7304be8c0b5eb1ae965e640d076e
Komo-Blows <156711146+Komo-Blows@users.noreply.github.com>
2026-01-31 03:08:15 -0500

    accessory spawning

---
+++ bubblesort/aesthetics_picker_resource.gd
@@ -0,0 +1,25 @@
+class_name aesthetics_picker
+extends Resource
+@export var techy: int = 0
+@export var fishy: int = 0
+@export var cutesy: int = 0
+@export var preppy: int = 0
+@export var ugly: int = 0
+@export var demonic: int = 0
+@export var cowboy: int = 0
+@export var irish: int = 0
+@export var badly_drawn: int = 0
+
+func get_dict() -> Dictionary:
+	# returns a global aesthetic-keyed dictionary
+	return {
+	Globals.aesthetics.techy: techy, 
+	Globals.aesthetics.fishy: fishy,
+	Globals.aesthetics.cutesy: cutesy,
+	Globals.aesthetics.preppy: preppy,
+	Globals.aesthetics.ugly: ugly,
+	Globals.aesthetics.demonic: demonic,
+	Globals.aesthetics.cowboy: cowboy,
+	Globals.aesthetics.irish: irish,
+	Globals.aesthetics.badly_drawn: badly_drawn,
+	}
