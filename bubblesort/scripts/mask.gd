2de5cf422bf7f9b269c52579cebc68f556d2d68e
Komo-Blows <156711146+Komo-Blows@users.noreply.github.com>
2026-01-31 15:38:07 -0500

    huge changes

--- bubblesort/scripts/mask.gd
+++ bubblesort/scripts/mask.gd
@@ -3,21 +3,24 @@ class_name mask
 
 @export var aesthetic: aesthetics_picker
 
-func add_accessory(accessory):
-	accessory.reparent(self)
 
 #current charm that is selected
 var focus_charm
 
 func _on_body_entered(body: Node2D) -> void:
-	print("got something")
 	if body is RigidBody2D:
-		print("picked up" + body.name)
 		focus_charm = body
 		focus_charm.over_mask = true
 
 func _on_body_exited(body: Node2D) -> void:
 	if body == focus_charm:
 		focus_charm.over_mask = false
-		print("dropped off" + body.name)
+		focus_charm = null
+
+func _process(_d):
+	if focus_charm and focus_charm.stuck:
+		focus_charm.reparent(self)
+		focus_charm.attach(self)
+		# add mask aesthetics to self
+		aesthetic.add(focus_charm.aesthetic)
 		focus_charm = null
