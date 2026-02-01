05e5c074528609afda098d72bd8137fc53ff70e6
Komo-Blows <156711146+Komo-Blows@users.noreply.github.com>
2026-02-01 11:29:43 -0500

    Update color_picker_button.gd

--- bubblesort/color_picker_button.gd
+++ bubblesort/color_picker_button.gd
@@ -6,12 +6,13 @@ var mouse_over:bool = false
 func _process(d):
 	if picked_up:
 		icon.global_position = get_global_mouse_position() + Vector2(27, -27)
+		var color := Color(0.5, 1, 0.5, 1) # get color at mouse position :cry:
+		icon.modulate = color
 		if Input.is_action_just_pressed('click'):
 			picked_up = false
-			# get color at mouse position :cry:
-			var color := Color(0.5, 1, 0.5, 1)
 			get_parent().color_mask(color)
 	else:
+		icon.modulate = Color(1, 1, 1, 1)
 		icon.position = lerp(icon.position, Vector2.ZERO, 0.1)
 		if get_local_mouse_position().length() < 55: # mouse over button
 			if Input.is_action_just_pressed('click'):
