1c4a648838d041c33b7ce0369c2a562f752b679c
Dimitry Pianykh <pianykh.d@northeastern.edu>
2026-02-01 15:11:42 -0500

    Added simple score text.

--- bubblesort/scripts/character.gd
+++ bubblesort/scripts/character.gd
@@ -1,3 +1,4 @@
+class_name CharacterHandler
 extends Node2D
 
 @onready var sprite = $sprite
--- bubblesort/scripts/root.gd
+++ bubblesort/scripts/root.gd
@@ -8,15 +8,18 @@ var animator = $AnimationPlayer
 var mask_menu = $"mask selector"
 
 var current_mask: MaskScene = null
-var points := 0
 @export var quota: int = 1
-@export var day: int = 0
+@export var daily_attempts: int = 5
+var attempt_count:int = 0
+var points := 0
+var day: int = 0
 var points_dict: Dictionary[CharacterHandler.SatisfactionLevel, int] = {
-	CharacterHandler.SatisfactionLevel.CHOPPED: -1,
-	CharacterHandler.SatisfactionLevel.UPSET: 0,
-	CharacterHandler.SatisfactionLevel.MEH: 1,
+	CharacterHandler.SatisfactionLevel.CHOPPED: 0,
+	CharacterHandler.SatisfactionLevel.UPSET: 1,
+	CharacterHandler.SatisfactionLevel.MEH: 2,
 	CharacterHandler.SatisfactionLevel.HAPPY: 3
 }
+var game_over: bool = false
 
 func new_character(delay : int = 2):
 	animator.play("slide out")
@@ -40,6 +43,7 @@ func _ready():
 		if i >= 10: # ten charms only
 			break
 	new_character()
+	update_points(day, points, quota)
 
 @onready
 var current_character = $character
@@ -53,8 +57,7 @@ func select_mask(resource): # connected as signal to mask_shape_selected
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("space") and current_mask:
-		await child.take_mask(current_mask).finished #Creates a reaction, and lets it finish.
-		new_character()
+		self.handle_spacebar()
 	if Input.is_action_just_pressed("screenshot"):
 		take_screenshot()
 	#$Camera2D.zoom += Vector2(0.001, 0.001)
@@ -75,11 +78,39 @@ func color_mask(c: Color):
 	if current_mask: 
 		current_mask.color(c)
 		
-## Given the current day and current quota, calculates the next quota.
+## Calculates the next quota.
 func calculate_next_quota() -> int:
 	return self.quota + 1
 
 ## Given the day, current amount of points, and a quota updates the points label.
 func update_points(new_day: int, new_points: int, new_quota: int):
-	$Points.text = 	"Day: {}, Points/Quota: {}".format([ \
+	self.day = new_day
+	self.points = new_points
+	self.quota = new_quota
+	$Points.text = 	"Day: {}, Points/Quota: {}/{}".format([ \
 	str(new_day), str(new_points), str(new_quota)], "{}")
+	
+## Calculates the next daily attempt amount.
+func calculate_next_daily_attempts() -> int:
+	return self.daily_attempts + 1
+	
+## Handles the space bar action
+func handle_spacebar() -> void:
+	await child.take_mask(current_mask).finished #Creates a reaction, and lets it finish.
+	# Check if the day is over.
+	if self.attempt_count >= self.daily_attempts:
+		if self.points >= self.quota:
+			self.daily_attemps = calculate_next_daily_attempts() # hidden from user.
+			self.update_points(self.day, 0, calculate_next_quota())
+		else:
+			self.game_over = true
+	if !self.game_over:
+		print(self.attempt_count)
+		print(self.game_over)
+		self.attempt_count += 1
+		self.points += self.points_dict[child.calculate_reaction(current_mask)]
+		new_character()
+	else:
+		print("Game over?")
+		$Points.text = "GAME OVER"
+		#TODO: prolly something else happens
