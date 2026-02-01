7774d035bb85cbf0091cc081ab2d438db6fbd217
Komo-Blows <156711146+Komo-Blows@users.noreply.github.com>
2026-02-01 11:36:42 -0500

    update color picker

--- bubblesort/color_picker_button.gd
+++ bubblesort/color_picker_button.gd
@@ -3,16 +3,19 @@ extends Node2D
 var picked_up:bool = false
 var mouse_over:bool = false
 @onready var icon = $colorpicker
+@onready var bg = $background
 func _process(d):
 	if picked_up:
 		icon.global_position = get_global_mouse_position() + Vector2(27, -27)
 		var color := Color(0.5, 1, 0.5, 1) # get color at mouse position :cry:
 		icon.modulate = color
+		bg.modulate = color
 		if Input.is_action_just_pressed('click'):
 			picked_up = false
 			get_parent().color_mask(color)
 	else:
 		icon.modulate = Color(1, 1, 1, 1)
+		bg.modulate = Color(1, 1, 1, 1)
 		icon.position = lerp(icon.position, Vector2.ZERO, 0.1)
 		if get_local_mouse_position().length() < 55: # mouse over button
 			if Input.is_action_just_pressed('click'):
